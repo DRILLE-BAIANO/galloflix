6f8dc4b673ad4e7e179692a5751c916e17e0ce8f
kauanunues <marykakinho@gmail.com>
2024-05-27 11:11:06 -0300

    Configuração do serviço do idntity, e codificação do metodo para popular usuario

--- GalloFlix/Data/AppDbContext.cs
+++ GalloFlix/Data/AppDbContext.cs
@@ -1,6 +1,8 @@
 
 using GalloFlix.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration.UserSecrets;
 
 namespace GalloFlix.Data;
 
@@ -9,7 +11,7 @@ public class AppDbContext : DbContext
    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
    {
    }
-
+   public DbSet<AppUser> AppUsers { get; set; }
    public DbSet<Genre> Genres { get; set; }
 
    public DbSet<Movie> Movies { get; set; }
@@ -20,6 +22,7 @@ public class AppDbContext : DbContext
       base.OnModelCreating(builder);
 
       #region Configuração Do Muitos para Muitos do MovieGenre
+
       builder.Entity<MovieGenre>()
            .HasKey(mg => new { mg.MovieId, mg.GenreId });
 
@@ -33,6 +36,99 @@ public class AppDbContext : DbContext
          .WithMany(g => g.Movies)
          .HasForeignKey(mg => mg.GenreId);
       #endregion
+   
+       #region Popular os dados de Usuarios
+      //Perfil - Identityrole
+      List<IdentityRole> roles = new()
+      {
+         new IdentityRole()
+         {
+            Id = Guid.NewGuid().ToString(),
+            Name = "Administrador",
+            NormalizedName = "ADIMINISTRADOR" 
+         },
+         new IdentityRole()
+         {
+            Id = Guid.NewGuid().ToString(),
+            Name = "Moderador",
+            NormalizedName = "MODERADOR" 
+         },
+         new IdentityRole()
+         {
+            Id = Guid.NewGuid().ToString(),
+            Name = "Usuário",
+            NormalizedName = "USUÁRIO" 
+         }
+      };
+      builder.Entity<IdentityRole>().HasData(roles);
+
+      // Conta de Usuário - IdentityUser
+      List<IdentityUser> users = new()
+      { 
+         new IdentityUser()
+         {
+            Id = Guid.NewGuid().ToString(),
+            Email = "admi@galloflix.com",
+            NormalizedEmail = "ADMIN@GALLOFLIX.COM",
+            UserName = "admin",
+            NormalizedUserName = "ADMIN",
+            EmailConfirmed = true,
+            LockoutEnabled = false,
+
+         },
+         new IdentityUser()
+         {
+            Id = Guid.NewGuid().ToString(),
+            Email = "user@gmail.com",
+            NormalizedEmail = "USER@GMAIL.COM",
+            UserName = "user",
+            NormalizedUserName = "USER",
+            EmailConfirmed = true,
+            LockoutEnabled = false,
+
+         }
+
+      };
+      foreach (var user in users)
+      {
+         PasswordHasher<IdentityUser> pass = new();
+         pass.HashPassword(user,"@Etec123");
+      }
+      builder.Entity<IdentityUser>().HasData(users);
+
+      // Dados Pessoais
+      List<AppUser> appUsers = new()
+      {
+         new AppUser()
+         {
+            AppUserId = users[0].Id,
+            Name = "Eduardo Henrique Santos Silva",
+            Birthday = DateTime.Parse("21/01/2008"),
+
+         },
+         new AppUser()
+         {
+            AppUserId = users[1].Id,
+            Name = "Baiano",
+            Birthday = DateTime.Parse("25/09/2000"),
+         }
+      };
+      builder.Entity<AppUser>().HasData();
+
+      List<IdentityUserRole> identityUserRoles = new()
+      {
+         new IdentityUserRole<string>;
+         {
+            UserId = Users,
+            RoleId = roles,
+         }
+         new IdentityUserRole<string>;
+         {
+            UserId = Users,
+            RoleId = roles,
+         }
+      }
+       #endregion   
    }
 
 }
